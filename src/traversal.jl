88af0f95729d19c66fd2b7021e83277c628e75a7
Justin Angevaare <jangevaa@uoguelph.ca>
2016-03-09 13:32:27 -0500

    first draft of postorder traversal algorithm

--- src/traversal.jl
+++ src/traversal.jl
@@ -4,8 +4,23 @@ Function to visit nodes of phylogenetic tree with postorder traversal
 function postorder(tree::Tree)
   visited = fill(false, length(tree.nodes))
   visit_order = Int64[]
+  next = 1
   while !all(visited)
-    # TODO
+    sub_visited = Bool[]
+    for i in tree.nodes[next].out_branches
+      if visited[tree.branches[i].target]
+        push!(sub_visited, true)
+      else
+        push!(sub_visited, false)
+      end
+    end
+    if all(sub_visited) || length(sub_visited) == 0
+      push!(visit_order, next)
+      visited[next] = true
+      next = tree.branches[tree.nodes[next].in_branches][1].source
+    else
+      next = tree.branches[tree.nodes[next].out_branches[!sub_visited][1]].target
+    end
   end
   return visit_order
 end
