096b07785df303bab3ac1db81adf81589b18d153
Justin Angevaare <jangevaa@uoguelph.ca>
2016-03-17 16:05:03 -0400

    allow for larger floating point errors in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,36 +6,36 @@ using Distributions
 # 1.1 JC69
 t = rand(Gamma(10.))
 a = JC69([1.])
-@test maximum(abs(expm(Q(a) * t) .- P(a, t))) < 1e-15
+@test maximum(abs(expm(Q(a) * t) .- P(a, t))) < 1e-14
 @test P(a, Inf) * rand(Multinomial(1, [0.25, 0.25, 0.25, 0.25])) == a.π
 
 # 1.2 K80
 @test Q(K80([1., 1.])) == Q(a)
 t = rand(Gamma(10.))
 b = K80([1., 2.])
-@test maximum(abs(expm(Q(b) * t) .- P(b, t))) < 1e-15
+@test maximum(abs(expm(Q(b) * t) .- P(b, t))) < 1e-14
 @test P(b, Inf) * rand(Multinomial(1, [0.25, 0.25, 0.25, 0.25])) == b.π
 
 # 1.3 F81
 @test Q(F81([4.], [0.25, 0.25, 0.25, 0.25])) == Q(a)
-π = rand(Dirichlet([0.25, 0.25, 0.25, 0.25]))
+πc = rand(Dirichlet([0.25, 0.25, 0.25, 0.25]))
 t = rand(Gamma(10.))
-c = F81([1.], π)
-@test maximum(abs(expm(Q(c) * t) .- P(c, t))) < 1e-15
+c = F81([1.], πc)
+@test maximum(abs(expm(Q(c) * t) .- P(c, t))) < 1e-14
 @test P(c, Inf) * rand(Multinomial(1, [0.25, 0.25, 0.25, 0.25])) == c.π
 
 # 1.4 F84
-π = rand(Dirichlet([0.25, 0.25, 0.25, 0.25]))
+πd = rand(Dirichlet([0.25, 0.25, 0.25, 0.25]))
 t = rand(Gamma(10.))
-d = F84([1., 2.], π)
-@test maximum(abs(expm(Q(d) * t) .- P(d, t))) < 1e-15
+d = F84([1., 2.], πd)
+@test maximum(abs(expm(Q(d) * t) .- P(d, t))) < 1e-14
 @test P(d, Inf) * rand(Multinomial(1, [0.25, 0.25, 0.25, 0.25])) == d.π
 
 # 1.5 HKY85
-π = rand(Dirichlet([0.25, 0.25, 0.25, 0.25]))
+πe = rand(Dirichlet([0.25, 0.25, 0.25, 0.25]))
 t = rand(Gamma(10.))
-e = HKY85([1., 2.], π)
-@test maximum(abs(expm(Q(e) * t) .- P(e, t))) < 1e-15
+e = HKY85([1., 2.], πe)
+@test maximum(abs(expm(Q(e) * t) .- P(e, t))) < 1e-14
 @test P(e, Inf) * rand(Multinomial(1, [0.25, 0.25, 0.25, 0.25])) == e.π
 
 # 1.6 TN93
