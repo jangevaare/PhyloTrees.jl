351b096b0eea7de1322ef365dc5dc2bb24cd9fd5
Justin Angevaare <jangevaa@uoguelph.ca>
2016-04-27 21:54:18 -0400

    utility maturation...

--- src/utilities.jl
+++ src/utilities.jl
@@ -11,6 +11,17 @@ end
 
 
 """
+Is a particular node a root?
+"""
+function isroot(tree::Tree, node::Int64)
+  if !(1 <= node <= length(tree.nodes))
+    error("Invalid node specified")
+  end
+  return isroot(tree.nodes[node])
+end
+
+
+"""
 Is a particular node a leaf?
 """
 function isleaf(node::Node)
@@ -23,6 +34,17 @@ end
 
 
 """
+Is a particular node a leaf?
+"""
+function isleaf(tree::Tree, node::Int64)
+  if !(1 <= node <= length(tree.nodes))
+    error("Invalid node specified")
+  end
+  return isleaf(tree.nodes[node])
+end
+
+
+"""
 Is a particular node an internal node?
 """
 function isnode(node::Node)
@@ -31,6 +53,17 @@ function isnode(node::Node)
   else
     return false
   end
+end
+
+
+"""
+Is a particular node an internal node?
+"""
+function isnode(tree::Tree, node::Int64)
+  if !(1 <= node <= length(tree.nodes))
+    error("Invalid node specified")
+  end
+  return isnode(tree.nodes[node])
 end
 
 
@@ -86,17 +119,60 @@ end
 
 
 """
+Check if a node index is valid
+"""
+function validnode(tree::Tree, node::Int64)
+  return 1 <= node <= length(tree.nodes)
+end
+
+
+"""
+Check if a node index is valid
+"""
+function validnode(tree::Tree, nodes::Vector{Int64})
+  return all(1 .<= nodes .<= length(tree.nodes))
+end
+
+
+"""
+Find the parent node of a specified node
+"""
+function parentnode(tree::Tree, node::Int64)
+  if !validnode(tree, node)
+    error("Invalid node specified")
+  end
+  return tree.branches[tree.nodes[node].in].source
+end
+
+
+"""
 Node pathway through which a specified node connects to a root
 """
 function nodepath(tree::Tree, node::Int64)
-  if !(1 <= node <= length(tree.nodes))
+  if !validnode(tree, node)
     error("Invalid node specified")
   end
   path = [node]
-  while !isroot(tree.nodes[pathway[end]])
-    push!(path, tree.branches[tree.nodes[pathway[end]].in].source)
+  while !isroot(tree, pathway[end])
+    push!(path, parentnode(tree, pathway[end]))
   end
   return path
+end
+
+
+"""
+The root associated with a specified node
+"""
+function noderoot(tree::Tree, node::Int64)
+  return nodepath(tree, node)[end]
+end
+
+
+"""
+Check for connectedness of two nodes
+"""
+function areconnected(tree::Tree, node1::Int64, node2::Int64)
+  return noderoot(tree, node1) == noderoot(tree, node2)
 end
 
 
@@ -104,29 +180,17 @@ end
 Node pathway through which two specified nodes connect
 """
 function nodepath(tree::Tree, node1::Int64, node2::Int64)
-  if !(1 <= node1 <= length(tree.nodes))
-    error("Invalid node specified")
-  end
-  if !(1 <= node2 <= length(tree.nodes))
+  if !validnode(tree, [node1; node2])
     error("Invalid node specified")
   end
   path1 = reverse(nodepath(tree, node1))
   path2 = reverse(nodepath(tree, node2))
-  if path1[1] !== path2[1]
+  if !areconnected(tree, node1, node2)
     error("Nodes are not connected")
-  else
-    minlength = minimum(length(path1), length(path2))
-    mrca_index = findlast(path1[1:minlength] .== path2[1:minlength])
-    return [reverse(path1[(mrca_index+1):end]); path2[mrca_index:end]]
   end
-end
-
-
-"""
-The root associated with a specified node
-"""
-function noderoot(tree:Tree, node::Int64)
-  return nodepath(tree, node)[end]
+  minlength = minimum(length(path1), length(path2))
+  mrcnode_index = findlast(path1[1:minlength] .== path2[1:minlength])
+  return [reverse(path1[(mrcnode_index+1):end]); path2[mrcnode_index:end]]
 end
 
 
@@ -134,12 +198,9 @@ end
 Branch pathway through which a specified node connects to a root
 """
 function branchpath(tree::Tree, node::Int64)
-  if !(1 <= node <= length(tree.nodes))
-    error("Invalid node specified")
-  end
   path = []
-  while !isroot(tree.nodes[node])
-    push!(path, tree.nodes[pathway[end]].in)
+  while !isroot(tree, node)
+    push!(path, tree.nodes[node].in)
     node = tree.branches[path[end]].in
   end
   return path
@@ -150,22 +211,16 @@ end
 Branch pathway through which two specified nodes connect
 """
 function branchpath(tree::Tree, node1::Int64, node2::Int64)
-  if !(1 <= node1 <= length(tree.nodes))
-    error("Invalid node specified")
-  end
-  if !(1 <= node2 <= length(tree.nodes))
-    error("Invalid node specified")
-  end
-  if noderoot(tree, node1) !== noderoot(tree, node2)
+  if !areconnected(tree, node1, node2)
     error("Nodes are not connected")
   end
   path1 = reverse(branchpath(tree, node1))
   path2 = reverse(branchpath(tree, node2))
   minlength = minimum(length(path1), length(path2))
   if minlength == 0
-    mrcb_index = 0
+    mrcbranch_index = 0
   else
-    mrcb_index = findlast(path1[1:minlength] .== path2[1:minlength])
+    mrcbranch_index = findlast(path1[1:minlength] .== path2[1:minlength])
   end
-  return [reverse(path1[(mrcb_index+1):end]); path2[mrcb_index+1:end]]
+  return [reverse(path1[(mrcbranch_index+1):end]); path2[mrcbranch_index+1:end]]
 end
