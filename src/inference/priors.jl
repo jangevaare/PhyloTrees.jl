4fa0e06aed4bf78b5b53af7fe2c57ff7887251de
Justin Angevaare <jangevaa@uoguelph.ca>
2016-05-17 14:20:06 -0400

    constructor functions for Prior type

--- src/inference/priors.jl
+++ src/inference/priors.jl
@@ -1,4 +1,15 @@
 type SubstitutionModelPrior
   Θ::Vector{UnivariateDistribution}
   π::Nullable{Dirichlet}
+
+  function SubstitutionModelPrior(Θ::Vector{UnivariateDistribution})
+    return new(Θ, Nullable{Dirichlet}())
+  end
+
+  function SubstitutionModelPrior(Θ::Vector{UnivariateDistribution}, π::Dirichlet)
+    if length(π.alpha) != 4
+      error("Invalid Dirichlet prior distribution specified")
+    end
+    return new(Θ, Nullable(π))
+  end
 end
