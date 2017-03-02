cb904d2fe070479e6ff2130ac49703e2060fb4ad
Justin Angevaare <jangevaa@uoguelph.ca>
2017-03-02 12:38:00 -0500

    add root-node distance (required for plotting)

--- src/distance.jl
+++ src/distance.jl
@@ -26,3 +26,21 @@ function distance(tree::Tree)
   leaves = findleaves(tree)
   return [distance(tree, i, j) for i in leaves, j in leaves]
 end
+
+
+"""
+distance(tree::Tree,
+         node::Int64)
+
+Distance between a `Node` and it's associated root
+"""
+function distance(tree::Tree,
+                  node1::Int64,
+                  node2::Int64)
+  path = branchpath(tree, node)
+  dist = 0.
+  for i in path
+    dist += tree.branches[i].length
+  end
+  return dist
+end
