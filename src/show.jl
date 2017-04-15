3a3ba6a4c0c0f65ddd9d03127e5c25e317713a1b
Richard Reeve <git@richardreeve.net>
2017-04-16 00:37:03 +0100

    Use interface for show().

--- src/show.jl
+++ src/show.jl
@@ -1,43 +1,42 @@
-function show(io::IO, object::Node)
-  if length(object.in) == 0
-    if length(object.out) > 0
-      for i in 1:length(object.out)
+function show(io::IO, object::AbstractNode)
+  if !hasinbound(object)
+    if countoutbounds(object) > 0
+      for (i, b) in zip(1:countoutbounds, getoutbounds(object))
         if i == 1
-          print(io, "\r\e[0m[\e[1mroot node\e[0m]-->[branch $(object.out[i])]")
+          print(io, "\r\e[0m[\e[1mroot node\e[0m]-->[branch $b]")
         elseif i < length(object.out)
-          print(io, "\n\e[0m$(repeat(" ", length("[root node]")))-->[branch $(object.out[i])]")
+          print(io, "\n\e[0m$(repeat(' ', length('[root node]')))-->[branch $b]")
         else
-          print(io, "\n\e[0m$(repeat(" ", length("[root node]")))-->[branch $(object.out[i])]")
+          print(io, "\n\e[0m$(repeat(' ', length('[root node]')))-->[branch $b]")
         end
       end
     else
       print(io, "\r\e[0m[\e[1munattached node\e[0m]")
     end
-  elseif length(object.in) == 1
-    if length(object.out) == 0
-      print(io, "\r\e[0m[branch $(object.in[1])]-->[\e[1mleaf node\e[0m]")
-    elseif length(object.in) == 1
-      for i in 1:length(object.out)
-        if length(object.out) == 1
-          print(io, "\r\e[0m[branch $(object.in[1])]-->[\e[1minternal node\e[0m]-->[branch $(object.out[i])]")
+  else
+    if countoutbounds(object) == 0
+      print(io, "\r\e[0m[branch $(getinbound(object))]-->[\e[1mleaf node\e[0m]")
+    elseif hasinbound(object)
+      for (i, b) in zip(1:countoutbounds, getoutbounds(object))
+        if countoutbounds(object) == 1
+          print(io, "\r\e[0m[branch $(getinbound(object))]-->[\e[1minternal node\e[0m]-->[branch $b]")
         elseif i == 1
-          print(io, "\r\e[0m[branch $(object.in[1])]-->[\e[1minternal node\e[0m]-->[branch $(object.out[i])]")
-        elseif i < length(object.out)
-          print(io, "\n\e[0m$(repeat(" ", length("[branch $(object.in[1])]-->[internal node]")))-->[branch $(object.out[i])]")
+          print(io, "\r\e[0m[branch $(getinbound(object))]-->[\e[1minternal node\e[0m]-->[branch $b]")
+        elseif i < countoutbounds(object)
+          print(io, "\n\e[0m$(repeat(' ', length('[branch $(getinbound(object))]-->[internal node]')))-->[branch $b]")
         else
-          print(io, "\n\e[0m$(repeat(" ", length("[branch $(object.in[1])]-->[internal node]")))-->[branch $(object.out[i])]")
+          print(io, "\n\e[0m$(repeat(' ', length('[branch $(getinbound(object))]-->[internal node]')))-->[branch $b]")
         end
       end
     end
   end
 end
 
-
 function show(io::IO, object::Branch)
-  print(io, "\r\e[0m[node $(object.source)]-->[\e[1m$(object.length) branch\e[0m]-->[node $(object.target)]")
+  print(io, "\r\e[0m[node $(getsource(object))]-->[\e[1m$(getlength(object)) branch\e[0m]-->[node $(gettarget(object))]")
 end
 
 
-function show(io::IO, object::Tree)
-  print(io, "\r\e[0mPhylogenetic tree with $(length(object.nodes)) nodes and $(length(object.branches)) branches")
+function show(io::IO, object::AbstractTree)
+  print(io, "\r\e[0mPhylogenetic tree with $(length(getnodes(object))) nodes and $(length(getbranches(object))) branches")
 end
