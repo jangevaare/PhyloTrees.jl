637eb3e02d16d99e2bdccc33ba81ca7b5224d18e
Richard Reeve <git@richardreeve.net>
2017-04-16 12:27:13 +0100

    Tidy up display of trees.

--- src/show.jl
+++ src/show.jl
@@ -1,42 +1,59 @@
-function show(io::IO, object::AbstractNode)
-  if !hasinbound(object)
-    if countoutbounds(object) > 0
-      for (i, b) in zip(1:countoutbounds, getoutbounds(object))
-        if i == 1
-          print(io, "\r\e[0m[\e[1mroot node\e[0m]-->[branch $b]")
-        elseif i < length(object.out)
-          print(io, "\n\e[0m$(repeat(' ', length('[root node]')))-->[branch $b]")
+function show(io::IO, object::AbstractNode, n::Nullable{Int} = Nullable{Int}())
+    node = "node"
+    if !isnull(n)
+        node *= " $(get(n))"
+    end
+    if !hasinbound(object)
+        if countoutbounds(object) > 0
+            blank = repeat(" ", length("[root $node]") + (isnull(n) ? 0 : 1))
+            for (i, b) in zip(1:countoutbounds(object), getoutbounds(object))
+                if countoutbounds(object) == 1
+                    print(io, "\e[0m[\e[1mroot $node\e[0m]-->[branch $b]")
+                elseif i == 1
+                    print(io, "\e[0m[\e[1mroot $node\e[0m]-->[branch $b]\n")
+                elseif i < countoutbounds(object)
+                    print(io, "\e[0m$blank-->[branch $b]\n")
+                else
+                    print(io, "\e[0m$blank-->[branch $b]")
+                end
+            end
         else
-          print(io, "\n\e[0m$(repeat(' ', length('[root node]')))-->[branch $b]")
+            print(io, "\e[0m[\e[1munattached $node\e[0m]")
         end
-      end
     else
-      print(io, "\r\e[0m[\e[1munattached node\e[0m]")
-    end
-  else
-    if countoutbounds(object) == 0
-      print(io, "\r\e[0m[branch $(getinbound(object))]-->[\e[1mleaf node\e[0m]")
-    elseif hasinbound(object)
-      for (i, b) in zip(1:countoutbounds, getoutbounds(object))
-        if countoutbounds(object) == 1
-          print(io, "\r\e[0m[branch $(getinbound(object))]-->[\e[1minternal node\e[0m]-->[branch $b]")
-        elseif i == 1
-          print(io, "\r\e[0m[branch $(getinbound(object))]-->[\e[1minternal node\e[0m]-->[branch $b]")
-        elseif i < countoutbounds(object)
-          print(io, "\n\e[0m$(repeat(' ', length('[branch $(getinbound(object))]-->[internal node]')))-->[branch $b]")
-        else
-          print(io, "\n\e[0m$(repeat(' ', length('[branch $(getinbound(object))]-->[internal node]')))-->[branch $b]")
+        if countoutbounds(object) == 0
+            print(io, "\e[0m[branch $(getinbound(object))]-->[\e[1mleaf $node\e[0m]")
+        elseif hasinbound(object)
+            blank = repeat(" ",
+                           length("[branch $(getinbound(object))]-->[internal $node]") +
+                           (isnull(n) ? 0 : 1))
+            for (i, b) in zip(1:countoutbounds(object), getoutbounds(object))
+                if countoutbounds(object) == 1
+                    print(io, "\e[0m[branch $(getinbound(object))]-->[\e[1minternal $node\e[0m]-->[branch $b]")
+                elseif i == 1
+                    print(io, "\e[0m[branch $(getinbound(object))]-->[\e[1minternal $node\e[0m]-->[branch $b]\n")
+                elseif i < countoutbounds(object)
+                    print(io, "\e[0m$blank-->[branch $b]\n")
+                else
+                    print(io, "\e[0m$blank-->[branch $b]")
+                end
+            end
         end
-      end
     end
-  end
+end
+
+function show{N <: AbstractNode}(io::IO, p::Pair{Int, N})
+    show(io, p[2], Nullable(p[1]))
 end
 
 function show(io::IO, object::Branch)
-  print(io, "\r\e[0m[node $(getsource(object))]-->[\e[1m$(getlength(object)) branch\e[0m]-->[node $(gettarget(object))]")
+    print(io, "\e[0m[node $(getsource(object))]-->[\e[1m$(getlength(object)) length branch\e[0m]-->[node $(gettarget(object))]")
 end
 
+function show(io::IO, p::Pair{Int, Branch})
+    print(io, "\e[0m[node $(getsource(p[2]))]-->[\e[1m$(getlength(p[2])) length branch $(p[1])\e[0m]-->[node $(gettarget(p[2]))]")
+end
 
 function show(io::IO, object::AbstractTree)
-  print(io, "\r\e[0mPhylogenetic tree with $(length(getnodes(object))) nodes and $(length(getbranches(object))) branches")
+    print(io, "\e[0mPhylogenetic tree with $(length(getnodes(object))) nodes and $(length(getbranches(object))) branches\n")
 end
