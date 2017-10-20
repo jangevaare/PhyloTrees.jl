9719cb975c37d6be88a3fe923511da761556ac28
Justin Angevaare <jangevaa@uoguelph.ca>
2017-10-20 14:05:15 -0400

    rid deprecated syntax in traversal algorithm
    rid deprecated syntax in traversal algorithm

--- src/traversal.jl
+++ src/traversal.jl
@@ -7,7 +7,7 @@ function postorder(tree::Tree)
   nodes = collect(keys(tree.nodes))
   visited = Dict{Int64, Bool}(i => false for i in nodes)
   visit_order = Int64[]
-  next = nodes[findfirst(!collect(values(visited)))]
+  next = nodes[findfirst(.!collect(values(visited)))]
   while !all(collect(values(visited)))
     sub_visited = Bool[]
     for i in tree.nodes[next].out
@@ -20,11 +20,11 @@ function postorder(tree::Tree)
         if length(tree.nodes[next].in) == 1
           next = tree.branches[tree.nodes[next].in[1]].source
         else
-          next = nodes[findfirst(!collect(values(visited)))]
+          next = nodes[findfirst(.!collect(values(visited)))]
         end
       end
     else
-      next = tree.branches[tree.nodes[next].out[!sub_visited][1]].target
+      next = tree.branches[tree.nodes[next].out[.!sub_visited][1]].target
     end
   end
   return visit_order
